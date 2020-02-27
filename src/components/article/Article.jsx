123604839efac41b5c09bccc039cf4e8c4471b6f
jkunciene <j.kunciene@gmail.com>
2020-02-27 13:54:13 +0200

    state

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,17 +1,38 @@
-import React from "react"
+import React, {Component} from "react"
 import './article.scss';
 
-function Article(props) {
-    return (
-        <div className="col">
-        <div className="contact-card">
-            <img src="http://placekitten.com/300/200"/>
-            <h3>{props.data.name}</h3>
-            <p>{props.data.text}</p>
-            <a href="#">Daigiau...</a>
-        </div>
-        </div>
-    )
+class Article extends Component {
+    constructor() {
+        super()
+        this.state = {
+            counter: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                counter: prevState.counter + 1
+            }
+        })
+    }
+
+
+    render() {
+        return (
+
+            <div className="col">
+                <div className="contact-card">
+                    <img src="http://placekitten.com/300/200"/>
+                    <h3>{this.props.data.name}</h3>
+                    <p>{this.props.data.text}</p>
+                    <p>Like: {this.state.counter}</p>
+                    <a onClick={this.handleClick} href="#">Like</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Article;
