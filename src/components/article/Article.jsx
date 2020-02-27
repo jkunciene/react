72202949b4411210b65719760b5ec60214671d6d
jkunciene <j.kunciene@gmail.com>
2020-02-27 11:34:15 +0200

    article

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import './article.scss';
+
+function Article() {
+    return (
+        <div className="contact-card">
+            <img src="http://placekitten.com/300/200"/>
+            <h3>Mr. Whiskerson</h3>
+            <p>Phone: (212) 555-1234</p>
+            <p>Email: mr.whiskaz@catnap.meow</p>
+        </div>
+    )
+}
+
+export default Article
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './main.scss';
+import Article from "../article/Article";
 
 function Main() {
     return (
+
         <main>
-            <p>mano main</p>
+          <div className="row">
+              <div className="col">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div className="col">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div className="col">
+                  <Article/>
+              </div>
+
+          </div>
         </main>
     );
 }
