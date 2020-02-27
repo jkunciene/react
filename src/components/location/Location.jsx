6233be4f2ed64b714f9c4134447ebac84cf25115
jkunciene <j.kunciene@gmail.com>
2020-02-27 14:46:14 +0200

    paimam is api places.name

---
+++ src/components/location/Location.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+
+function Location(props) {
+    return (
+        <div className="container">
+            <p>{props.places}</p>
+
+        </div>
+    );
+}
+
+export default Location;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,21 +2,45 @@ import React, {Component} from 'react';
 
 import './main.scss';
 import Article from "../article/Article";
-import postai from "../../data/postai"
+import postai from "../../data/postai";
+import Location from "../location/Location";
 
 //react.component jei virsuje neimpoirtuoju
 
 class Main extends Component{
+    constructor() {
+        super()
+        this.state = {
+            places: [] //tuscias state vietovems
+        }
+    }
+
+    componentDidMount() {
+        //gaunami duomenys iš API
+        fetch("https://api.meteo.lt/v1/places")
+            .then(response => response.json())
+            .then(
+                data=>{
+                    this.setState({
+                        places: data
+                    })
+                    console.log(this.state.places);
+                }
+            )
+    }
+
     render(){
         const postData = postai.map(post=> <Article data={{
             key:post.id,
             title:post.pavadinimas,
             text:post.tekstas
         }} />)
+        const postLocation = this.state.places.map(places=> <Location places={places.name}/>)
         return(
             <main>
                 <div className="row">
                     {postData}
+                    {postLocation}
                 </div>
 
             </main>
